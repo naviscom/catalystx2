3de325e42654a65c05587fa481568cf022160746
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-02 22:19:01 +0500

    "commit from apiwriter"

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,13 +1,7 @@
 -- name: CreateUser :one
 INSERT INTO users (
-    username,
-    hashed_password,
-    full_name,
-    email,
-    password_changed_at,
-    created_at
 ) VALUES (
- $1, $2, $3, $4, $5, $6
+ 
 )
 RETURNING *;
 
