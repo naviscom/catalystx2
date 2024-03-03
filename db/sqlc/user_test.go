cf16ca24c38bff7b988cd58e1e93bf8f5b86c851
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-03 15:27:28 +0500

    "commit from apiwriter"

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -19,9 +19,12 @@ func createRandomUser(t *testing.T) User {
 	user, err := testStore.CreateUser(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
+	require.Equal(t, arg.Username, user.Username)
 	require.Equal(t, arg.HashedPassword, user.HashedPassword)
 	require.Equal(t, arg.FullName, user.FullName)
 	require.Equal(t, arg.Email, user.Email)
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
 	return user
 }
 
