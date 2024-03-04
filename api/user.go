56a334ff871705e715bca8e0facacdabf600720f
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-04 17:06:06 +0500

    "commit from apiwriter"

---
+++ api/user.go
@@ -0,0 +1,150 @@
+package api
+
+import (
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+	db "github.com/naviscom/catalystx2/db/sqlc"
+)
+
+type createUserRequest struct {
+	HashedPassword	string	`json:"hashed_password" binding:"required"`
+	FullName	string	`json:"full_name" binding:"required"`
+	Email	string	`json:"email" binding:"required"`
+	PasswordChangedAt	time.Time	`json:"password_changed_at" binding:"required"`
+	PasswordCreatedAt	time.Time	`json:"password_created_at" binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		HashedPassword:	req.HashedPassword,
+		FullName:	req.FullName,
+		Email:	req.Email,
+		PasswordChangedAt:	req.PasswordChangedAt,
+		PasswordCreatedAt:	req.PasswordCreatedAt,
+	}
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+type getUserRequest0 struct {
+	Username	string	`uri:"username" binding:"required,min=1"`
+}
+
+func (server *Server) getUser0(ctx *gin.Context) {
+	var req getUserRequest0
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := server.store.GetUser0(ctx, req.Username)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+type getUserRequest1 struct {
+	Email	string	`uri:"email" binding:"required,min=1"`
+}
+
+func (server *Server) getUser1(ctx *gin.Context) {
+	var req getUserRequest1
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := server.store.GetUser1(ctx, req.Email)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+type listUserRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize   int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listUsers(ctx *gin.Context) {
+	var req listUserRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListUsersParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	users, err := server.store.ListUsers(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+type updateUserRequest struct {
+	Username	string	`json:"username" binding:"required"`
+	HashedPassword	string	`json:"hashed_password" binding:"required"`
+	FullName	string	`json:"full_name" binding:"required"`
+	Email	string	`json:"email" binding:"required"`
+	PasswordChangedAt	time.Time	`json:"password_changed_at" binding:"required"`
+	PasswordCreatedAt	time.Time	`json:"password_created_at" binding:"required"`
+}
+
+func (server *Server) updateUser(ctx *gin.Context) {
+	var req updateUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.UpdateUserParams{
+		Username:	req.Username,
+		HashedPassword:	req.HashedPassword,
+		FullName:	req.FullName,
+		Email:	req.Email,
+		PasswordChangedAt:	req.PasswordChangedAt,
+		PasswordCreatedAt:	req.PasswordCreatedAt,
+	}
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+type deleteUserRequest struct {
+	Usernamefunc (server *Server) deleteUser(ctx *gin.Context) {
+	var req deleteUserRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteUser(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, "record deleted successfully")
+}
+
