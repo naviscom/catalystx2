cb5a42f7db52943657791d6d786802958eb0ea6a
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-05 12:35:48 +0500

    "commit from apiwriter"

--- util/password.go
+++ util/password.go
@@ -1,25 +1,21 @@
 package util
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
+	"fmt"
 	"golang.org/x/crypto/bcrypt"
 
 )
 
-func TestPassword(t *testing.T) {
-	password := RandomString(6)
-	hashedPassword1, err := HashPassword(password)
-	require.NoError(t, err)
-	require.NotEmpty(t, hashedPassword1)
-	err = CheckPassword(password, hashedPassword1)
-	require.NoError(t, err)
-	wrongPassword := RandomString(6)
-	err = CheckPassword(wrongPassword, hashedPassword1)
-	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
-	hashedPassword2, err := HashPassword(password)
-	require.NoError(t, err)
-	require.NotEmpty(t, hashedPassword2)
-	require.NotEqual(t, hashedPassword1, hashedPassword2)
+// HashPassword returns the bcrypt hash of the password.
+func HashPassword(password string) (string , error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(hashedPassword), nil
+}
+
+// CheckPassword checks if the provided password is correct or not
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
