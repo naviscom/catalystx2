cbd043480dc1b1e53879a0ee340d17e4b8dc0cda
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-20 08:07:16 +0500

    "commit from apiwriter"

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -53,21 +53,21 @@ func TestExpiredJWTToken(t *testing.T) {
 	require.Nil(t, payload)
 }
 
-//func TestInvalidJWTTokenAlgNone(t *testing.T) {
-//	payload, err := NewPayload(util.RandomOwner(), util.DepositorRole, time.Minute)
-	//payload, err := NewPayload(util.RandomName(8), time.Minute)
-	//require.NoError(t, err)
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	//payload, err := NewPayload(util.RandomOwner(), util.DepositorRole, time.Minute)
+	payload, err := NewPayload(util.RandomName(8), time.Minute)
+	require.NoError(t, err)
 
-	//jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
-	//token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
-	//require.NoError(t, err)
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
 
-	//maker, err := NewJWTMaker(util.RandomString(32))
-	//require.NoError(t, err)
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
 
-	//payload, err = maker.VerifyToken(token)
-	//require.Error(t, err)
-	//require.EqualError(t, err, ErrInvalidToken.Error())
-	//require.Nil(t, payload)
-//}
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
 
