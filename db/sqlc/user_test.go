920fb2e9a9250f980618fadc39136a6e7dc8dcd7
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-20 07:38:19 +0500

    another manual commit

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -96,6 +96,7 @@ func TestUpdateUser(t *testing.T) {
 	require.NotEmpty(t, user2)
 
 	require.Equal(t, user1.Username, user2.Username)
+	
 	require.Equal(t, arg.HashedPassword, user2.HashedPassword)
 	require.Equal(t, arg.FullName, user2.FullName)
 	require.Equal(t, arg.Email, user2.Email)
