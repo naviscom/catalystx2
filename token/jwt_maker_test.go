a382b68841fbfbfd898be290e79ba11ecc466781
Ghaz Riz <ghaz.riz@naviscom.com>
2024-03-20 08:02:55 +0500

    "commit from apiwriter"

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -15,13 +15,13 @@ func TestJWTMaker(t *testing.T) {
 	require.NoError(t, err)
 
 	username := util.RandomName(8)
-//	role := util.DepositorRole
+	//role := util.DepositorRole
 	duration := time.Minute
 
-issuedAt := time.Now()
-expiredAt := issuedAt.Add(duration)
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
 
-//	token, payload, err := maker.CreateToken(username, role, duration)
+	//token, payload, err := maker.CreateToken(username, role, duration)
 	token, err := maker.CreateToken(username, duration)
 	require.NoError(t, err)
 	require.NotEmpty(t, token)
@@ -33,7 +33,7 @@ expiredAt := issuedAt.Add(duration)
 
 	require.NotZero(t, payload.ID)
 	require.Equal(t, username, payload.Username)
-//	require.Equal(t, role, payload.Role)
+	//require.Equal(t, role, payload.Role)
 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 }
@@ -41,8 +41,8 @@ func TestExpiredJWTToken(t *testing.T) {
 	maker, err := NewJWTMaker(util.RandomString(32))
 	require.NoError(t, err)
 
-//token, payload, err := maker.CreateToken(util.RandomName(8), util.DepositorRole, -time.Minute)
-token, err := maker.CreateToken(util.RandomName(8), -time.Minute)
+	//token, payload, err := maker.CreateToken(util.RandomName(8), util.DepositorRole, -time.Minute)
+	token, err := maker.CreateToken(util.RandomName(8), -time.Minute)
 	require.NoError(t, err)
 	require.NotEmpty(t, token)
 	//require.NotEmpty(t, payload)
